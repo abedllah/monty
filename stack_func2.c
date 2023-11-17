e633b295c89a54cfdac2480910a66adf80f1b125
abdellah <abdellahtaghouni@gmai.com>
2023-11-17 00:00:21 +0000

    nop

--- stack_func2.c
+++ stack_func2.c
@@ -5,10 +5,10 @@
  * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_num)
 {
 	(void)stack;
-	(void)line_number;
+	(void)line_num;
 }
 
 
