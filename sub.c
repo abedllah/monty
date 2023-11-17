7083b8c856735ee0ffed39387e64bdc226f376d7
abdellah <abdellahtaghouni@gmai.com>
2023-11-17 00:31:49 +0000

    fix

--- sub.c
+++ sub.c
@@ -5,7 +5,7 @@
  * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
-void sub(stack_t **stack, unsigned int line_number)
+void sub_nodes(stack_t **stack, unsigned int line_number)
 {
 	int sum;
 
